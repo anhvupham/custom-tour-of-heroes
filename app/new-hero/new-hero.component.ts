37d6699df0ef30086983c841cb0807a6db31af5c
Vu Pham <vincent.pham.us@gmail.com>
2016-04-28 16:55:29 -0700

    1st commit

---
+++ app/new-hero/new-hero.component.ts
@@ -0,0 +1,28 @@
+import {Component} from "angular2/core";
+import {HeroService} from "../services/hero/hero.service"
+import {Hero} from "../classes/hero/hero";
+
+@Component({
+    selector: "new-hero",
+    template: `
+		<h3>Create new hero</h3>
+		<input #name (keyup.enter)="create(name.value)" />
+		<button (click)="create(name.value)">Create</button>
+		`
+})
+
+export class NewHeroComponent {
+    constructor(
+        private _heroService: HeroService
+    ) { }
+    create(name: string) {
+        this._heroService.getHeroes().then(heroes => {
+            let id = heroes[heroes.length - 1].id + 1;
+            let newhero: Hero = {
+                id: id,
+                name: name
+            };
+			this._heroService.newHero(newhero);
+        });
+    }
+}
---
+++ app/services/hero/hero.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from "angular2/core";
+import {Hero} from "../../classes/hero/hero";
+import {HEROES} from "../../data/mock-heroes"
+
+@Injectable()
+export class HeroService {
+    getHeroes() {
+        return Promise.resolve(HEROES);
+    }
+    getHeroesSlowly() {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+        );
+    }
+    getHero(id: number) {
+        return Promise.resolve(HEROES).then(heroes => heroes.filter(hero => hero.id === id)[0]);
+    }
+    newHero(hero: Hero) {
+		HEROES.push(hero);
+    }
+}
